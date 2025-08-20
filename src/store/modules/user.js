fb94a6d6c1a388bcda91274a9e80ce586c05532a
Francisco Topolansky <franciscotopolansky@gmail.com>
2025-08-20 17:04:32 -0300

    TMDB API Integration

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,7 +5,8 @@ const state = {
     id: null,
     name: null,
     email: null,
-    partnerId: null
+    partnerId: null,
+    movieApiPage: 1
 }
 
 //Para modificar el estado "state". Recibe un payload y asigno campos al estado.
@@ -14,12 +15,15 @@ const mutations = {
         state.id = payload.id;
         state.name = payload.name;
         state.email = payload.email;
-        state.partnerId = payload.partnerId
+        state.partnerId = payload.partnerId;
+        state.movieApiPage = payload.movieApiPage
     },
     SET_PARTNER_ID: (state, payload) => {
         state.partnerId = payload;
     },
-    
+    SET_MOVIE_API_PAGE: (state, payload) => {
+        state.movieApiPage = payload;
+    },
     CLEAR_USER_DATA: (state) => {
         state.id = null;
         state.name = null;
@@ -34,7 +38,9 @@ const actions = {
     setPartnerId(context, id) {
         context.commit("SET_PARTNER_ID", id);
     },
-
+    setMovieApiPage(context, pageNumber) {
+        context.commit("SET_MOVIE_API_PAGE", pageNumber)
+    },
     logout(context) {
         context.commit('CLEAR_USER_DATA');
         localStorage.removeItem("my-app"); //Importante limpiar de la memoria del navegador, sino guarda el usuario.
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="text-center">
     <h1>Bienvenido a la página de inicio</h1>
@@ -7,7 +6,26 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HomeView'
-}
+  name: "HomeView",
+  data: () => ({
+    isLoading: false,
+    movies: [],
+    currentMovie: {},
+    currentIndex: 0,
+  }),
+  methods: {
+    async fetchMovie(page) {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&page=${page}&sort_by=popularity.desc&vote_average.gte=3`
+      );
+
+      if (res.data && res.data.results.length > 0) {
+        this.movies = res.data.results;
+      }
+    },
+  },
+};
 </script>
